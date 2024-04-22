603679a80c0bb9ac841529fc914f6281a4b4e8c5
ANNA BILLSTROM <annabillstrom@annas-air.lan>
2024-04-21 21:14:52 -0700

    Filters fixed

--- components/Filters.js
+++ components/Filters.js
@@ -39,6 +39,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginBottom: 16,
+    marginLeft: 20,
   },
   filterText: {
     fontFamily: 'KarlaExtraBold',
