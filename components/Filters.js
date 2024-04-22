552ebc2ef0d7ce3f479a83bea26fefc83ec93ba6
ANNA BILLSTROM <annabillstrom@annas-air.lan>
2024-04-21 21:11:26 -0700

    Filter changes

--- components/Filters.js
+++ components/Filters.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
   filterText: {
     fontFamily: 'KarlaExtraBold',
     fontSize: 13,
-    textTransform: 'uppercase',
+    textTransform: 'capitalize',
   }
 });
 
